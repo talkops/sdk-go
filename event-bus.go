0d68478ea19acdcab0f6d46678b4e7c012040e66
bierdok <2333728+bierdok@users.noreply.github.com>
2025-05-18 12:18:14 +0200

    update

--- event-bus.go
+++ event-bus.go
@@ -1,53 +1,81 @@
 package talkops
 
 import (
-	_ "embed"
+	"bufio"
 	"encoding/json"
 	"fmt"
-	"github.com/r3labs/sse/v2"
+	"net"
+	"os"
 	"reflect"
+	"time"
 )
 
-type Subscriber struct {
+type EventBus struct {
+	useState func() map[string]interface{}
 	useConfig func() map[string]interface{}
+	lastEventState string
+	client net.Conn
 }
 
-func NewSubscriber(useConfig func() map[string]interface{}) *Subscriber {
-	config := useConfig()
-	mercure := config["mercure"].(map[string]interface{})
-	topic := mercure["subscriber"].(map[string]interface{})["topic"].(string)
-	url := mercure["url"].(string)
-	token := mercure["subscriber"].(map[string]interface{})["token"].(string)
-	client := sse.NewClient(fmt.Sprintf("%s?topic=%s", url, topic))
-	if token != "" {
-		client.Headers["Authorization"] = fmt.Sprintf("Bearer %s", token)
-	}
-	s := &Subscriber{
+func NewEventBus(useState func() map[string]interface{}, useConfig func() map[string]interface{}) *EventBus {
+	conn, _ := net.Dial("unix", os.Getenv("TALKOPS_SOCKET"))
+	eb := &EventBus{
+		useState: useState,
 		useConfig: useConfig,
+		client: conn,
 	}
-	client.SubscribeRaw(func(msg *sse.Event) {
-		var event map[string]interface{}
-		if err := json.Unmarshal(msg.Data, &event); err != nil {
-			return
-		}
-		s.onEvent(event)
+	go eb.PublishEvent(map[string]interface{}{
+		"type": "init",
 	})
-	return s
+	go eb.publishStatePeriodically()
+	scanner := bufio.NewScanner(eb.client)
+	for scanner.Scan() {
+		var event map[string]interface{}
+		if err := json.Unmarshal(scanner.Bytes(), &event); err == nil {
+			go eb.onEvent(event)
+		}
+	}
+	return eb
 }
 
-func (s *Subscriber) onEvent(event map[string]interface{}) {
-	config := s.useConfig()
+func (eb *EventBus) PublishState() {
+	event := map[string]interface{}{
+		"type": "state",
+		"state": eb.useState(),
+	}
+	eventJSON, _ := json.Marshal(event)
+	eb.lastEventState = string(eventJSON)
+	eb.PublishEvent(event)
+}
+
+func (eb *EventBus) PublishEvent(event map[string]interface{}) {
+	data, _ := json.Marshal(event)
+	eb.client.Write(data) // data is well transmitted
+}
+
+func (eb *EventBus) publishStatePeriodically() {
+	for {
+		time.Sleep(500 * time.Millisecond)
+		event := map[string]interface{}{
+			"type": "state",
+			"state": eb.useState(),
+		}
+		eventJSON, _ := json.Marshal(event)
+		lastEventState := string(eventJSON)
+		if eb.lastEventState != lastEventState {
+			eb.lastEventState = lastEventState
+			go eb.PublishEvent(event)
+		}
+	}
+}
+
+func (eb *EventBus) onEvent(event map[string]interface{}) {
+	config := eb.useConfig()
 	callbacks, _ := config["callbacks"].(map[string]reflect.Value)
 	functions, _ := config["functions"].(map[string]reflect.Value)
 	parameters, _ := config["parameters"].([]*Parameter)
-	publisher, _ := config["publisher"].(*Publisher)
 	eventType, _ := event["type"].(string)
 	switch eventType {
-	case "ping":
-		if publisher != nil {
-			publisher.OnPing()
-		}
-		return
 	case "function_call":
 		name, _ := event["name"].(string)
 		args, _ := event["args"].(map[string]interface{})
@@ -61,7 +89,7 @@ func (s *Subscriber) onEvent(event map[string]interface{}) {
 				if len(result) > 0 {
 					event["output"] = result[0].Interface()
 				}
-				publisher.PublishEvent(event)
+				eb.PublishEvent(event)
 			}()
 		}
 		return
@@ -70,8 +98,6 @@ func (s *Subscriber) onEvent(event map[string]interface{}) {
 		for name, value := range params {
 			for i := range parameters {
 				if parameters[i].Name == name {
-					fmt.Println(parameters[i].Name)
-					fmt.Println(value)
 					if value == nil {
 						parameters[i].SetValue("")
 					} else {
@@ -86,7 +112,7 @@ func (s *Subscriber) onEvent(event map[string]interface{}) {
 				ready = false
 			}
 		}
-		publisher.PublishState()
+		eb.PublishState()
 		if !ready {
 			return
 		}
--- extension.go
+++ extension.go
@@ -5,7 +5,6 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"os"
 	"reflect"
 	"runtime/debug"
 	"strings"
@@ -42,35 +41,27 @@ func getSdkVersion() string {
 }
 
 type Extension struct {
-	callbacks         map[string]reflect.Value
-	category          string
-	demo              bool
-	features          []string
-	functions         map[string]reflect.Value
-	functionSchemas   []map[string]interface{}
-	icon              string
+	callbacks  map[string]reflect.Value
+	category string
+	eventBus *EventBus
+	demo bool
+	features []string
+	functions map[string]reflect.Value
+	functionSchemas []map[string]interface{}
+	icon string
 	installationSteps []string
-	instructions      string
-	name              string
-	parameters        []*Parameter
-	publisher         *Publisher
-	softwareVersion   string
-	started           bool
-	token             string
-	website           string
+	instructions string
+	name string
+	parameters []*Parameter
+	softwareVersion string
+	started bool
+	token string
+	website string
 }
 
 func NewExtension() *Extension {
 	return &Extension{
 		callbacks: make(map[string]reflect.Value),
-		token: os.Getenv("TALKOPS_TOKEN"),
-	}
-}
-
-func NewExtensionFromToken(token string) *Extension {
-	return &Extension{
-		callbacks: make(map[string]reflect.Value),
-		token: token,
 	}
 }
 
@@ -199,35 +190,28 @@ func (e *Extension) Start() *Extension {
 	var mercure map[string]interface{}
 	json.Unmarshal(decoded, &mercure)
 	version := getSdkVersion()
-	if os.Getenv("ENV") == "development" {
-		NewManifest(func() map[string]interface{} {
-			return map[string]interface{}{
-				"category": e.category,
-				"demo": e.demo,
-				"features": e.features,
-				"icon": e.icon,
-				"name": e.name,
-				"sdk": map[string]interface{}{
-					"name": "go",
-					"version": version,
-				},
-				"softwareVersion": e.softwareVersion,
-				"website": e.website,
-			}
-		})
-		NewReadme(func() map[string]interface{} {
-			return map[string]interface{}{
-				"features": e.features,
-				"name": e.name,
-			}
-		})
-	}
-	e.publisher = NewPublisher(
-		func() map[string]interface{} {
-			return map[string]interface{}{
-				"mercure": mercure,
-			}
-		},
+	NewManifest(func() map[string]interface{} {
+		return map[string]interface{}{
+			"category": e.category,
+			"demo": e.demo,
+			"features": e.features,
+			"icon": e.icon,
+			"name": e.name,
+			"sdk": map[string]interface{}{
+				"name": "go",
+				"version": version,
+			},
+			"softwareVersion": e.softwareVersion,
+			"website": e.website,
+		}
+	})
+	NewReadme(func() map[string]interface{} {
+		return map[string]interface{}{
+			"features": e.features,
+			"name": e.name,
+		}
+	})
+	e.eventBus = NewEventBus(
 		func() map[string]interface{} {
 			return map[string]interface{}{
 				"category": e.category,
@@ -245,30 +229,29 @@ func (e *Extension) Start() *Extension {
 				"functionSchemas": e.functionSchemas,
 			}
 		},
+		func() map[string]interface{} {
+			return map[string]interface{}{
+				"callbacks": e.callbacks,
+				"functions": e.functions,
+				"parameters": e.parameters,
+			}
+		},
 	)
-	NewSubscriber(func() map[string]interface{} {
-		return map[string]interface{}{
-			"callbacks": e.callbacks,
-			"extension": e,
-			"functions": e.functions,
-			"mercure": mercure,
-			"parameters": e.parameters,
-			"publisher": e.publisher,
-		}
-	})
 	return e
 }
 
 func (e *Extension) EnableAlarm() *Extension {
-	if e.publisher != nil {
-		e.publisher.PublishEvent(map[string]interface{}{"type": "alarm"})
+	if e.eventBus != nil {
+		e.eventBus.PublishEvent(map[string]interface{}{
+			"type": "alarm",
+		})
 	}
 	return e
 }
 
 func (e *Extension) SendMedias(medias []*Media) *Extension {
-	if e.publisher != nil {
-		e.publisher.PublishEvent(map[string]interface{}{
+	if e.eventBus != nil {
+		e.eventBus.PublishEvent(map[string]interface{}{
 			"type": "medias",
 			"medias": medias,
 		})
@@ -277,8 +260,8 @@ func (e *Extension) SendMedias(medias []*Media) *Extension {
 }
 
 func (e *Extension) SendMessage(text string) *Extension {
-	if e.publisher != nil {
-		e.publisher.PublishEvent(map[string]interface{}{
+	if e.eventBus != nil {
+		e.eventBus.PublishEvent(map[string]interface{}{
 			"type": "message",
 			"text": text,
 		})
@@ -287,8 +270,8 @@ func (e *Extension) SendMessage(text string) *Extension {
 }
 
 func (e *Extension) SendNotification(text string) *Extension {
-	if e.publisher != nil {
-		e.publisher.PublishEvent(map[string]interface{}{
+	if e.eventBus != nil {
+		e.eventBus.PublishEvent(map[string]interface{}{
 			"type": "notification",
 			"text": text,
 		})
